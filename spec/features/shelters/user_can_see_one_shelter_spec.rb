255257d228d1900c64e4e46e8fdd6965ba329bce
Leah Riffell <leah.riffell@gmail.com>
2020-08-19 16:11:29 -0600

    Add test for asserting that a link to edit shelter exists on a show shelter by id page

--- spec/features/shelters/user_can_see_one_shelter_spec.rb
+++ spec/features/shelters/user_can_see_one_shelter_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "show shelter by id page", type: :feature do 
-  it "can see all shelter's name and address" do
+  it "can see the shelter's name and address" do
     shelter_1 = Shelter.create(id: 1,
                                # is adding the id num above what I should be doing? 
                                name: "Rocky Mountain Puppy Rescue",
@@ -31,5 +31,19 @@ RSpec.describe "show shelter by id page", type: :feature do
     visit "/shelters/1"
 
     expect(page).to have_link(href: "/shelters")
+  end
+
+  it "can link to form for updating its attributes" do
+    shelter_1 = Shelter.create(id: 1,
+                               # is adding the id num above what I should be doing? 
+                               name: "Rocky Mountain Puppy Rescue",
+                               address: "10021 E Iliff Ave",
+                               city: "Aurora",
+                               state: "CO",
+                               zip: "80247")
+
+    visit "/shelters/1"
+
+    expect(page).to have_link(href: "/shelters/1/edit")
   end
 end
