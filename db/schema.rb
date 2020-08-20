fd8ca24c0bb75c7b183b5899076c0878149e1251
Leah Riffell <leah.riffell@gmail.com>
2020-08-20 17:49:14 -0600

    Add relationship migration for many pets to one shelter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_224717) do
+ActiveRecord::Schema.define(version: 2020_08_20_234535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_08_20_224717) do
     t.string "name"
     t.string "approximate_age"
     t.string "sex"
+    t.bigint "shelter_id"
+    t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
   create_table "shelters", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema.define(version: 2020_08_20_224717) do
     t.string "zip"
   end
 
+  add_foreign_key "pets", "shelters"
 end
