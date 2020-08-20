75a835fee5a1329c82935896044589f3022b9293
Leah Riffell <leah.riffell@gmail.com>
2020-08-19 21:42:39 -0600

    Update destroy path to route to shelter/:id instead of /shelter as shelter id would not be findable

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
 
   get '/shelters/:id/edit', to:'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
-  delete '/shelters', to: 'shelters#destroy'
+  delete '/shelters/:id', to: 'shelters#destroy'
 end
