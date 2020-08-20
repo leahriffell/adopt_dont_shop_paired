217459b3d4ffaed45d592af084c7e102cd3d5dd3
Leah Riffell <leah.riffell@gmail.com>
2020-08-19 18:45:03 -0600

    Update typo in route for shelters#update path and HTTP verb

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   get '/shelters/:id', to: 'shelters#show'
 
   get '/shelters/:id/edit', to:'shelters#edit'
-  post '/shelters/:id', to: 'shelters#create'
+  patch '/shelters/:id', to: 'shelters#update'
 end
