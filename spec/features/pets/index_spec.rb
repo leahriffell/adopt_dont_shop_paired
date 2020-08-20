6a4198bed0de9de6468072af6822f4a04108d196
Leah Riffell <leah.riffell@gmail.com>
2020-08-20 17:23:28 -0600

    Create variable for @pets within pets index controller and update image test

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -20,11 +20,12 @@ RSpec.describe "pets index page", type: :feature do
   it "can see all pets and their info" do
     visit "/pets"
 
-    expect(page).to have_content(@pet_1.image)
+    expect(page).to have_css("img[src*='http://3.bp.blogspot.com/-72agMABPgDw/Tx-76OX1SWI/AAAAAAAAAB4/OYmSC3j-4S8/s400/5.jpg']")
     expect(page).to have_content(@pet_1.name)
     expect(page).to have_content(@pet_1.approximate_age)
     expect(page).to have_content(@pet_1.sex)
-    expect(page).to have_content(@pet_2.image)
+    
+    expect(page).to have_css("img[src*='https://i.pinimg.com/564x/2e/94/aa/2e94aaff89dcf73b17de85b17cddc038.jpg']")
     expect(page).to have_content(@pet_2.name)
     expect(page).to have_content(@pet_2.approximate_age)
     expect(page).to have_content(@pet_2.sex)
