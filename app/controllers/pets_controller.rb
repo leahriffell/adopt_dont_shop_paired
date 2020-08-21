803f07015e1fd921c4c031cca6e9bf1208c6996e
Leah Riffell <leah.riffell@gmail.com>
2020-08-20 21:41:28 -0600

    Add create controller for new pet w/ default shelter_id and adoption_status

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,4 +6,26 @@ class PetsController < ApplicationController
   def show 
     @pet = Pet.find(params[:id])
   end
+
+  def new 
+    @pet = Pet.new
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def create 
+    shelter = Shelter.find(params[:id])
+
+    pet = Pet.new({
+      name: params[:name],
+      description: params[:description],
+      approximate_age: params[:approximate_age],
+      sex: params[:sex],
+      shelter_id: shelter.id,
+      adoption_status: "Adoptable"
+    })
+
+    pet.save
+
+    redirect_to "/shelters/#{shelter.id}/pets"
+  end
 end
