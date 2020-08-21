280de1c64e903a16dfbcd02937bb119aa7d6bd02
Leah Riffell <leah.riffell@gmail.com>
2020-08-20 22:49:39 -0600

    Add destroy action to pets controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -49,4 +49,10 @@ class PetsController < ApplicationController
 
     redirect_to "/pets/#{pet.id}"
   end
+
+  def destroy 
+    Pet.destroy(params[:id])
+      
+    redirect_to "/pets"
+  end
 end
