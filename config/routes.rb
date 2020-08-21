1093ad0a3906e99f8b17dc4720a818674cb8812e
Leah Riffell <leah.riffell@gmail.com>
2020-08-20 18:44:07 -0600

    Add test and route for shelter's pets page

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,13 @@ Rails.application.routes.draw do
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
   post '/shelters', to: 'shelters#create'
-
+  
+  get '/pets', to: 'pets#index'
   # this route must come later or else app will think that any path after /shelters/ is an id and will try and use the show action when we don't want it to (ex: new)
   get '/shelters/:id', to: 'shelters#show'
-
   get '/shelters/:id/edit', to:'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
-
-  get '/pets', to: 'pets#index'
+  get '/shelters/:id/pets', to:'shelters#pets'
 
 end
---
+++ spec/features/shelters/user_can_see_shelters_pets_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "show a shelter's pets page", type: :feature do
+  before :each do 
+    @shelter_1 = Shelter.create!(
+                                  name: "Rocky Mountain Puppy Rescue",
+                                  address: "10021 E Iliff Ave",
+                                  city: "Aurora",
+                                  state: "CO",
+                                  zip: "80247"
+                                )
+    
+    @pet_1 = Pet.create!(
+                          image: "http://3.bp.blogspot.com/-72agMABPgDw/Tx-76OX1SWI/AAAAAAAAAB4/OYmSC3j-4S8/s400/5.jpg",
+                          name: "Fluffy",
+                          approximate_age: "15 weeks",
+                          sex: "Female",
+                          shelter_id: @shelter_1.id
+                        )
+  end
+
+  it "can see all pets at a shelter" do
+    visit "/shelters/#{@shelter_1.id}/pets"
+
+    expect(page).to have_css("img[src*='http://3.bp.blogspot.com/-72agMABPgDw/Tx-76OX1SWI/AAAAAAAAAB4/OYmSC3j-4S8/s400/5.jpg']")
+    expect(page).to have_content(@pet_1.name)
+    expect(page).to have_content(@pet_1.approximate_age)
+    expect(page).to have_content(@pet_1.sex)
+  end
+end
