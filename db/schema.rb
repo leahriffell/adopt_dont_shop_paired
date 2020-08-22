df147a133a3222af80d0bc58286ed6220a1004b0
Leah Riffell <leah.riffell@gmail.com>
2020-08-22 10:30:34 -0600

    Add default value of adoptable to new pet's adoption status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_024438) do
+ActiveRecord::Schema.define(version: 2020_08_22_162336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2020_08_21_024438) do
     t.string "sex"
     t.bigint "shelter_id"
     t.string "description"
-    t.string "adoption_status"
+    t.string "adoption_status", default: "Adoptable"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
