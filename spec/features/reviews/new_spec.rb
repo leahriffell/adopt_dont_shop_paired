242885de517e5576d51e287c50296155abdcb992
Leah Riffell <leah.riffell@gmail.com>
2020-08-25 07:50:50 -0600

    Add flash message spec for new review

--- spec/features/reviews/new_spec.rb
+++ spec/features/reviews/new_spec.rb
@@ -27,4 +27,16 @@ RSpec.describe "new review page", type: :feature do
     expect(page).to have_css("img[src*=heart-4]")
     # how to make this test dynamic?
   end
+
+  it "can require fields" do
+    visit "/shelters/#{@shelter_1.id}/add_review"
+
+    fill_in(:title, with: "Mountains of Love <3!")
+    fill_in(:rating, with: 5)
+
+    click_button("Add Review")
+
+    expect(page).to have_content("Please fill out title, review, and content.")
+    expect(page).to have_current_path("/shelters/#{@shelter_1.id}/add_review")
+  end
 end
