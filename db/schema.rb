97e5ce385c1496335e3d9edbba1d1511636042d2
Dani Coleman <danicoleman00@gmail.com>
2020-08-25 18:38:08 -0600

    Add column favorites to pet table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_001549) do
+ActiveRecord::Schema.define(version: 2020_08_26_003455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_08_25_001549) do
     t.bigint "shelter_id"
     t.string "description"
     t.string "adoption_status", default: "Adoptable"
+    t.boolean "favorites"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
