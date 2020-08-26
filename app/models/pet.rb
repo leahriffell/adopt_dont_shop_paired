b62c7f144ae6ed33db10f5237d2f05e1b9f2db5c
Dani Coleman <danicoleman00@gmail.com>
2020-08-26 16:09:17 -0600

    Use new method from favorites list class

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,8 +1,8 @@
 class Pet < ApplicationRecord
   belongs_to :shelter
 
-  def self.favorite_count
-    self.where(favorites: true).count 
+  def self.favorite_pets
+    self.where(favorites: true)
   end
 
   def add_to_favorites
