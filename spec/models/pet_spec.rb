e238429036fc2c66b8a2646caf24b75cd811028e
Dani Coleman <danicoleman00@gmail.com>
2020-08-25 18:49:52 -0600

    Create class method to count all of the favorites

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -4,4 +4,25 @@ describe Pet, type: :model do
   describe 'relationships' do
     it { should belong_to :shelter }
   end
+  describe "class methods" do
+    it "can get number of favorited pets" do
+      @shelter_1 = Shelter.create!(
+                                    name: "Rocky Mountain Puppy Rescue",
+                                    address: "10021 E Iliff Ave",
+                                    city: "Aurora",
+                                    state: "CO",
+                                    zip: "80247"
+                                  )
+
+      @pet_1 = Pet.create!(
+                            image: "http://3.bp.blogspot.com/-72agMABPgDw/Tx-76OX1SWI/AAAAAAAAAB4/OYmSC3j-4S8/s400/5.jpg",
+                            name: "Fluffy",
+                            approximate_age: "15 weeks",
+                            sex: "Female",
+                            shelter_id: @shelter_1.id,
+                            favorites: true
+                          )
+      expect(Pet.favorite_count).to eq(1)
+    end                   
+  end
 end
