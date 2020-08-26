e5e4d741401abdee32eb5ee87fc92478fdfcf9f0
Dani Coleman <danicoleman00@gmail.com>
2020-08-26 15:31:47 -0600

    Create test for favorited pets

---
+++ spec/features/favorites/index_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'favorites index page' do
+  before :each do
+    @shelter_1 = Shelter.create!(
+                                  name: "Rocky Mountain Puppy Rescue",
+                                  address: "10021 E Iliff Ave",
+                                  city: "Aurora",
+                                  state: "CO",
+                                  zip: "80247"
+                                )
+
+    @pet_1 = Pet.create!(
+                          image: "http://3.bp.blogspot.com/-72agMABPgDw/Tx-76OX1SWI/AAAAAAAAAB4/OYmSC3j-4S8/s400/5.jpg",
+                          name: "Fluffy",
+                          approximate_age: "15 weeks",
+                          sex: "Female",
+                          shelter_id: @shelter_1.id,
+                          description: "I am fluffy and so cute. I need someone to be my friend forever!!",
+                          adoption_status: "adoptable",
+                          favorites: true
+                        )
+  end
+
+  it 'see all pets favorited' do
+    visit '/favorites'
+
+    within('#pet-1') do
+      expect(page).to have_link(@pet_1.name)
+      expect(page).to have_css("img[src*='http://3.bp.blogspot.com/-72agMABPgDw/Tx-76OX1SWI/AAAAAAAAAB4/OYmSC3j-4S8/s400/5.jpg']")
+    end
+
+  end
+
+end
