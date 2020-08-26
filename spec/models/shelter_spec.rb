5c7544bd7ab86e716805b1bc102897c41a3ef41a
Leah Riffell <leah.riffell@gmail.com>
2020-08-26 13:27:09 -0600

    Consolidate relationships tests in shelter spec

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -3,9 +3,6 @@ require 'rails_helper'
 describe Shelter, type: :model do
   describe 'relationships' do
     it { should have_many :pets }
-  end
-
-  describe 'relationships' do
     it { should have_many :reviews }
   end
 
