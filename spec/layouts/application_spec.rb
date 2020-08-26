03d9ef0d608d8655856c87be692ae746a6f477c2
Dani Coleman <danicoleman00@gmail.com>
2020-08-25 18:33:09 -0600

    Create test for adding favorites and their count in navigation bar

---
+++ spec/layouts/application_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "application", type: :feature do
+  before :each do
+    @shelter_1 = Shelter.create!(
+                                  name: "Rocky Mountain Puppy Rescue",
+                                  address: "10021 E Iliff Ave",
+                                  city: "Aurora",
+                                  state: "CO",
+                                  zip: "80247"
+                                )
+
+    @pet_1 = Pet.create!(
+                          image: "http://3.bp.blogspot.com/-72agMABPgDw/Tx-76OX1SWI/AAAAAAAAAB4/OYmSC3j-4S8/s400/5.jpg",
+                          name: "Fluffy",
+                          approximate_age: "15 weeks",
+                          sex: "Female",
+                          shelter_id: @shelter_1.id,
+                          favorite: true
+                        )
+  end
+  it "has navigation bar" do
+    visit "/"
+
+    expect(page).to have_content("Pets")
+    expect(page).to have_content("Shelters")
+    expect(page).to have_content("Favorites(1)")
+  end
+end
