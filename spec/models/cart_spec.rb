34ed88e91cccc870bc1e7b86a61d5e6825d54646
Dani Coleman <danicoleman00@gmail.com>
2020-08-27 16:23:07 -0600

    Add test for total count and add pet for our add pet to favorites cart

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe Cart do
 
   describe "#total_count" do
     it "can calculate the total number of pets it holds" do
+      cart = Cart.new({
+        '1' => 1,
+        '2' => 1
+      })
       expect(cart.total_count).to eq(2)
     end
   end
