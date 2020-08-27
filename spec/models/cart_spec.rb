f2b4cfa66d0f3a7837998aa15580de0e093bb1ef
Dani Coleman <danicoleman00@gmail.com>
2020-08-27 14:40:59 -0600

    Create test for cart to add pets to favorite

---
+++ spec/models/cart_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Cart do
+  subject { Cart.new({'1' => 1, '2' => 1}) }
+
+  describe "#total_count" do
+    it "can calculate the total number of pets it holds" do
+      expect(cart.total_count).to eq(2)
+    end
+  end
+
+  describe "#add_pet" do
+    it "adds a pet to its contents" do
+      cart = Cart.new({
+        '1' => 1,
+        '2' => 1
+      })
+
+      subject.add_pet(1)
+      subject.add_pet(2)
+
+      expect(subject.contents).to eq({'1' => 2, '2' => 2})
+    end
+  end
+end
