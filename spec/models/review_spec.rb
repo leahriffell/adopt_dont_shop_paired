63a58bbe4d3bef9201a8ad5c2006db8fd456380d
Leah Riffell <leah.riffell@gmail.com>
2020-08-28 15:38:48 -0600

    Add test to see if review has an image or not

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,4 +4,26 @@ describe Review, type: :model do
   describe 'relationships' do
     it { should belong_to :shelter }
   end
+
+  describe 'instance methods' do 
+    before :each do 
+      @review_1 = @shelter.reviews.create!(
+                                            title: "Mountains of Love! <3",
+                                            rating: 5,
+                                            content: "Super clean, well-facilitated, and healthy pups.", 
+                                            optional_picture: "https://static.boredpanda.com/blog/wp-content/uuuploads/tuna-funny-dog-tunameltsmyheart/tuna-funny-dog-tunameltsmyheart-4.jpg"
+                                          )
+
+      @review_2 = @shelter.reviews.create!(
+                                            title: "meh",
+                                            rating: 2,
+                                            content: "it was weird."
+                                          )
+    end
+
+    describe 'it can determine if review has an image or not' do
+      expect(@review_1.has_image).to eq(true)
+      expect(@review_2.has_image).to eq(false)
+    end
+  end
 end
