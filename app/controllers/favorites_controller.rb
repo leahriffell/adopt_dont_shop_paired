248bf55cb8abda383a4dc71c3167dab180dc4bb5
Dani Coleman <danicoleman00@gmail.com>
2020-08-28 17:36:22 -0600

    Remove unneeded text

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,8 +3,4 @@ class FavoritesController < ApplicationController
   def index
     @fav_pets = Pet.where(id: cart.contents.keys.map {|key| key.to_i })
   end
-
-  # def remove
-  #
-  # end
-end
+end 
