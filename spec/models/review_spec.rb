5ab6f3daede8c87b42da07ca7e1f1600bca70a2e
Leah Riffell <leah.riffell@gmail.com>
2020-08-28 15:51:34 -0600

    Add method for determining if review has an image or not

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -7,21 +7,29 @@ describe Review, type: :model do
 
   describe 'instance methods' do 
     before :each do 
-      @review_1 = @shelter.reviews.create!(
+      @shelter_1 = Shelter.create!(
+                                    name: "Rocky Mountain Puppy Rescue",
+                                    address: "10021 E Iliff Ave",
+                                    city: "Aurora",
+                                    state: "CO",
+                                    zip: "80247"
+                                  )
+
+      @review_1 = @shelter_1.reviews.create!(
                                             title: "Mountains of Love! <3",
                                             rating: 5,
                                             content: "Super clean, well-facilitated, and healthy pups.", 
                                             optional_picture: "https://static.boredpanda.com/blog/wp-content/uuuploads/tuna-funny-dog-tunameltsmyheart/tuna-funny-dog-tunameltsmyheart-4.jpg"
                                           )
 
-      @review_2 = @shelter.reviews.create!(
+      @review_2 = @shelter_1.reviews.create!(
                                             title: "meh",
                                             rating: 2,
                                             content: "it was weird."
                                           )
     end
 
-    describe 'it can determine if review has an image or not' do
+    it 'can determine if review has an image or not' do
       expect(@review_1.has_image).to eq(true)
       expect(@review_2.has_image).to eq(false)
     end
