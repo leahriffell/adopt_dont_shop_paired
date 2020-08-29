ed1d3a51fdb79025601e5c72009aa70703d8dd30
Dani Coleman <danicoleman00@gmail.com>
2020-08-29 15:06:59 -0600

    Add flash message to require all fields to submit an application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,23 +4,23 @@ class ApplicationsController < ApplicationController
     @application = Application.new
   end
 
-  def create 
+  def create
     application = Application.create(app_params)
-    pet_ids = params[:pet_names].map {|id| id.to_i}
-    pets = pet_ids.map {|id| Pet.find(id)}
-    pets.each {|pet| pet.applications << application}
-    pets.each {|pet| ApplicationPet.create(pet_id: pet.id, application_id: application.id)}
-    pets.each {|pet| cart.remove_pet(pet.id)}
-    if application.save
+    if application.save && params[:pet_names] != nil
+      pet_ids = params[:pet_names].map {|id| id.to_i}
+      pets = pet_ids.map {|id| Pet.find(id)}
+      pets.each {|pet| pet.applications << application}
+      pets.each {|pet| ApplicationPet.create(pet_id: pet.id, application_id: application.id)}
+      pets.each {|pet| cart.remove_pet(pet.id)}
       redirect_to "/favorites"
       flash[:success] = "Your application has been submitted for selected pets"
     else
-      # flash[:notice] = "Please fill out title, review, and content."
-      # redirect_to "/shelters/#{shelter.id}/reviews/new"
+      flash[:notice] = "Please fill out all fields."
+      redirect_to "/applications/new"
     end
   end
 
-  private 
+  private
 
   def app_params
     params.permit(:name, :address, :city, :state, :zip, :phone_number, :description)
