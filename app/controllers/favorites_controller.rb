bec1a4b13815521c5b8ffdba20cc31fc5a67f95f
Dani Coleman <danicoleman00@gmail.com>
2020-08-29 16:53:05 -0600

    Add instance var for pets with apps to index action

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,6 +2,6 @@ class FavoritesController < ApplicationController
 
   def index
     @fav_pets = Pet.where(id: cart.contents.keys.map {|key| key.to_i })
-    @pets = Pet.pets_with_apps
+    @pets_with_apps = Pet.pets_with_apps
   end
 end
