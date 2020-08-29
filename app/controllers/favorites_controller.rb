fa5e44ee7a88b36dbb340f6343f12b00a44024c3
Dani Coleman <danicoleman00@gmail.com>
2020-08-29 16:06:36 -0600

    Add method that get all pets with an application

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,5 +2,6 @@ class FavoritesController < ApplicationController
 
   def index
     @fav_pets = Pet.where(id: cart.contents.keys.map {|key| key.to_i })
+    @pets = Pet.pets_with_apps
   end
-end 
+end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,4 +2,8 @@ class Pet < ApplicationRecord
   belongs_to :shelter
   has_many :application_pets
   has_many :applications, through: :application_pets
+
+  def self.pets_with_apps
+    Pet.joins(:applications)
+  end
 end
