0f367f0de02a3f0a3ef808f9b8abf3ba83f9d37a
Leah Riffell <leah.riffell@gmail.com>
2020-08-29 13:07:07 -0600

    Create new spec and relationships for Pets table

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,5 @@
 class Pet < ApplicationRecord
   belongs_to :shelter
-  
+  has_many :application_pets
+  has_many :applications, through: :application_pets
 end
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 describe Pet, type: :model do
   describe 'relationships' do
     it { should belong_to :shelter }
+    it { should have_many :application_pets }
+    it {should have_many(:applications).through(:application_pets)}
   end
 end
