f241f553d22a086e1abe488dc94da1e560f7918c
Leah Riffell <leah.riffell@gmail.com>
2020-08-29 11:57:02 -0600

    Remove commented out code

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationsController < ApplicationController
   def new
-    # @fav_pet_ids = cart.favorite_pets.map {|id| id.to_i}
     @fav_pets = cart.favorite_pets.map {|id| Pet.find(id.to_i)}
   end
 end
