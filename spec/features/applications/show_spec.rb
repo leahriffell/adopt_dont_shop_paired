449e2c78a4105bd69b9fb111b9e86002da246ec4
Dani Coleman <danicoleman00@gmail.com>
2020-08-30 09:20:07 -0600

    Add test to display application info on app show page

---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+describe 'application show page' do
+  before :each do
+    @shelter_1 = Shelter.create!(
+                                  name: "Rocky Mountain Puppy Rescue",
+                                  address: "10021 E Iliff Ave",
+                                  city: "Aurora",
+                                  state: "CO",
+                                  zip: "80247"
+                                )
+
+    @pet_1 = @shelter_1.pets.create!(
+                                  image: "http://3.bp.blogspot.com/-72agMABPgDw/Tx-76OX1SWI/AAAAAAAAAB4/OYmSC3j-4S8/s400/5.jpg",
+                                  name: "Fluffy",
+                                  approximate_age: "15 weeks",
+                                  sex: "Female",
+                                  description: "I am fluffy and so cute. I need someone to be my friend forever!!",
+                                  adoption_status: "adoptable"
+                        )
+
+    @pet_2 = @shelter_1.pets.create!(
+                                  image: "https://qph.fs.quoracdn.net/main-qimg-775e3ef3258895374fcd610163d8c8e3",
+                                  name: "Turquoise",
+                                  approximate_age: "1 year",
+                                  sex: "Male",
+                                  description: "I am looking for green pastures to roam and play in.",
+                                  adoption_status: "adoptable"
+                                    )
+    @application = @pet_1.applications.create!(
+                          name: "Dani Coleman",
+                          address: "123 Road Dr.",
+                          city: "Arvada",
+                          state: "CO",
+                          zip: "80005",
+                          phone_number: "555-555-5555",
+                          description: "I love animals!!!!!"
+                        )
+
+    visit "/pets/#{@pet_1.id}"
+    click_link "Add to Favorites"
+
+    visit "/pets/#{@pet_2.id}"
+    click_link "Add to Favorites"
+  end
+
+  it 'can see application with all fields and name of pets' do
+
+    visit "/applications/#{@application.id}"
+
+    expect(page).to have_content(@application.name)
+    expect(page).to have_content(@application.address)
+    expect(page).to have_content(@application.city)
+    expect(page).to have_content(@application.state)
+    expect(page).to have_content(@application.zip)
+    expect(page).to have_content(@application.phone_number)
+    expect(page).to have_content(@application.description)
+    expect(page).to have_content(@application.pets.name)
+  end
+
+
+end
