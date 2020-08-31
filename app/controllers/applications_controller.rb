451dd547f7df21b093173c879c2d493847630405
Leah Riffell <leah.riffell@gmail.com>
2020-08-30 20:47:52 -0600

    Define pets that don't have pending apps to use in the view

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -22,6 +22,7 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
     @pets = @application.pets
+    @non_pending_pets = @pets.find_all {|pet| !pet.has_approved_applicant }
   end
 
   private
