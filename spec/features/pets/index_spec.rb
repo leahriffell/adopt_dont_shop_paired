1d6fd762ba4acb7447ee9d2eb0ffc50b5d05df6f
Dani Coleman <danicoleman00@gmail.com>
2020-08-31 17:24:58 -0600

    Add test to not delete pet with pending app; add logic for above in view

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "pets index page", type: :feature do 
+RSpec.describe "pets index page", type: :feature do
   before :each do
     @shelter_1 = Shelter.create!(
                                   name: "Rocky Mountain Puppy Rescue",
@@ -15,15 +15,17 @@ RSpec.describe "pets index page", type: :feature do
                           name: "Fluffy",
                           approximate_age: "15 weeks",
                           sex: "Female",
-                          shelter_id: @shelter_1.id
+                          shelter_id: @shelter_1.id,
+                          adoption_status: "Adoptable"
                         )
-    
+
     @pet_2 = Pet.create!(
                           image: "https://i.pinimg.com/564x/2e/94/aa/2e94aaff89dcf73b17de85b17cddc038.jpg",
                           name: "Bernard",
                           approximate_age: "1",
                           sex: "Male",
-                          shelter_id: @shelter_1.id
+                          shelter_id: @shelter_1.id,
+                          adoption_status: "Pending"
                         )
   end
 
@@ -45,11 +47,11 @@ RSpec.describe "pets index page", type: :feature do
     expect(page).to have_content(@pet_2.shelter.name)
   end
 
-  it "can link to one pet" do 
+  it "can link to one pet" do
     visit "/pets"
 
     expect(page).to have_link(href: "/pets/#{@pet_1.id}")
-    expect(page).to have_link(href: "/pets/#{@pet_2.id}")  
+    expect(page).to have_link(href: "/pets/#{@pet_2.id}")
   end
 
   it "can link to form for editing each pet" do
@@ -61,9 +63,19 @@ RSpec.describe "pets index page", type: :feature do
   it "can delete each pet" do
     visit "/pets"
 
-    first(".delete-pet").click
-
-    expect(current_path).to eq("/pets") 
+    within("#pet-#{@pet_1.id}") do
+      click_link "Delete Pet"
+    end
+    expect(current_path).to eq("/pets")
     expect(page).to_not have_content(@pet_1.name)
   end
+
+  it "cannot delete pet with approved application" do
+    visit "/pets"
+    within("#pet-#{@pet_2.id}") do
+      expect(page).to have_no_link("Delete Pet")
+      expect(page).to have_content("Pet has pending application")
+    end
+  end
+
 end
