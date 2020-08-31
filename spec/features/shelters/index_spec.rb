16736df2bf42a5a023bdba03277ccd369178eac9
Dani Coleman <danicoleman00@gmail.com>
2020-08-31 14:47:50 -0600

    Add test remove delete button for shelters with pending apps

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "shelters index page", type: :feature do 
+RSpec.describe "shelters index page", type: :feature do
   before :each do
     @shelter_1 = Shelter.create!(
                                   name: "Rocky Mountain Puppy Rescue",
@@ -9,7 +9,7 @@ RSpec.describe "shelters index page", type: :feature do
                                   state: "CO",
                                   zip: "80247"
                                 )
-    
+
     @shelter_2 = Shelter.create!(
                                   name: "Espiritu Alpacas",
                                   address: "8221 S Blue Creek Rd",
@@ -17,32 +17,64 @@ RSpec.describe "shelters index page", type: :feature do
                                   state: "CO",
                                   zip: "80439"
                                 )
-    
-    visit "/shelters"
+    @pet_2 = Pet.create!(
+                          image: "https://i.pinimg.com/564x/2e/94/aa/2e94aaff89dcf73b17de85b17cddc038.jpg",
+                          name: "Bernard",
+                          approximate_age: "1",
+                          sex: "Male",
+                          shelter_id: @shelter_2.id,
+                          adoption_status: "Pending",
+                          approved_applicant: "Paul Bunyan"
+                        )
+    @application = @pet_2.applications.create!(
+                                name: "Dani Coleman",
+                                address: "123 Road Dr.",
+                                city: "Arvada",
+                                state: "CO",
+                                zip: "80005",
+                                phone_number: "555-555-5555",
+                                description: "I love animals!!!!!"
+                              )
   end
 
   it "can see all shelters' names" do
+    visit "/shelters"
     expect(page).to have_content(@shelter_1.name)
     expect(page).to have_content(@shelter_2.name)
   end
 
   it "can can link to one shelter" do
+    visit "/shelters"
     expect(page).to have_link(href: "/shelters/#{@shelter_1.id}")
     expect(page).to have_link(href: "/shelters/#{@shelter_2.id}")
   end
 
   it "can link to form for creating a new shelter" do
+    visit "/shelters"
     expect(page).to have_link(href: "/shelters/new")
   end
 
   it "can link to form for editing each shelter" do
+    visit "/shelters"
     expect(page).to have_link(href: "/shelters/#{@shelter_1.id}/edit")
   end
 
   it "can delete each shelter" do
-    first(".delete-shelter").click
+    visit "/shelters"
 
-    expect(current_path).to eq("/shelters")    
-    expect(page).to_not have_content(@shelter_1.name)
+    within("#shelter-#{@shelter_1.id}") do
+      click_link "Delete Shelter"
+    end
+    expect(current_path).to eq("/shelters")
+    expect(page).to have_no_content(@shelter_1.name)
+  end
+
+  it "can only delete shelters with no pending applications" do
+    visit "/shelters"
+    
+    within("#shelter-#{@shelter_2.id}") do
+      expect(page).to have_no_link("Delete Shelter")
+      expect(page).to have_content("Shelter has pending applications")
+    end
   end
 end
