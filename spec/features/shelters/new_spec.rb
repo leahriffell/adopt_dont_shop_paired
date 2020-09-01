e278245c3e2a7bdecad234fe4bf84ca9d434e339
Dani Coleman <danicoleman00@gmail.com>
2020-09-01 15:09:42 -0600

    Add flash message for specific fields in new shelter

--- spec/features/shelters/new_spec.rb
+++ spec/features/shelters/new_spec.rb
@@ -1,29 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "new shelter page", type: :feature do 
+RSpec.describe "new shelter page", type: :feature do
   it "can create new shelter" do
     visit "/shelters/new"
-    
+
     fill_in(:name, with: "Kitty Rescue")
     fill_in(:address, with: "123 Whisker Lane")
     fill_in(:city, with: "Denver")
     fill_in(:state, with: "CO")
     fill_in(:zip, with: "80202")
     click_button("Add Shelter")
-    
+
     expect(page).to have_current_path("/shelters")
     expect(page).to have_content("Kitty Rescue")
-    # how to make this test dynamic?
   end
 
-  it "can require fields" do
+  it "can display error message for missing fields" do
     visit "/shelters/new"
-
     click_button("Add Shelter")
-
-    expect(page).to have_content("Please fill out all fields.")    
-    expect(page).to have_button("Add Shelter")
-
     expect(page).to have_current_path("/shelters/new")
+    expect(page).to have_content("Please fill in: name, address, city, state, zip")
   end
 end
