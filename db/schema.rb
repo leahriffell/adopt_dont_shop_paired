f6dc594b7ba40806a6d14f89f359173d89ea04b1
Leah Riffell <leah.riffell@gmail.com>
2020-09-03 10:24:28 -0600

    Create users table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_30_191658) do
+ActiveRecord::Schema.define(version: 2020_09_03_160141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,11 @@ ActiveRecord::Schema.define(version: 2020_08_30_191658) do
     t.string "city"
     t.string "state"
     t.string "zip"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
   end
 
   add_foreign_key "application_pets", "applications"
