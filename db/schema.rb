278175537e3fa19d3e435adbb9986294a2d6b378
Leah Riffell <leah.riffell@gmail.com>
2020-09-08 15:41:00 -0600

    Add attribute of role to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_160141) do
+ActiveRecord::Schema.define(version: 2020_09_08_213803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2020_09_03_160141) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.integer "role", default: 0
   end
 
   add_foreign_key "application_pets", "applications"
